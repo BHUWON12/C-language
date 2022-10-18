5cc04334d817a2d22857cc6680203f14ad4f54a7
BHUWON12 <bhandaribh12@gamil.com>
2022-10-18 23:38:38 +0530

    switch case

---
+++ switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int rating;
+printf("enter your rating(1-5)\n");
+scanf("%d",&rating);
+switch(rating){
+    case 1: 
+    printf("your rating is 1\n");
+    break;
+     case 2: 
+    printf("your rating is 2\n");
+    break;
+     case 3: 
+    printf("your rating is 3\n");
+    break;
+     case 4:
+    printf("your rating is 4\n");
+    break;
+     case 5: 
+    printf("your rating is 5\n");
+    break;
+    default :
+    printf("invalid rating\n");
+    break;
+}
+return 0;
+}
