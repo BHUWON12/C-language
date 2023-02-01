655c683eb9490665394b3fda106f8f6f51206273
BHUWON12 <bhandaribh12@gamil.com>
2023-02-02 00:11:02 +0530

    hi

---
+++ PST/lab1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define PI 3.141
+int main()
+{
+int r;
+float area,circum;
+printf("enter radious to find circumfrence and area of a circle\n");
+scanf("%d",&r);
+printf("the circumfrance of area is %f\n and the area of circle is  %f\n",2*PI*r,PI*r*r);
+
+return 0;
+}
